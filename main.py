e2677a9168ccf2c59ef0ff3ce353fbdb20e5ad41
DevC <dimabmwmw@mail.ru>
2023-11-18 19:08:54 +0300

    initial and added text file

---
+++ main.py
@@ -0,0 +1,9 @@
+# Если нет папки venv виртуальное окружение
+# python -m venv venv - создание папки venv
+# Активация venv .\venv\Script\activate
+# Создание Git репозиторий git init
+# Добавить файл git add [файл] или .
+# Удалить git rm
+# Клонировать репозиторий
+# git clone
+# git checkout -b initial создание ветки
